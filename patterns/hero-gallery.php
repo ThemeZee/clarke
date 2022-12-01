a1273f409f80a464ffbe600cec87bc9c3c5d8c53
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2022-12-01 11:49:19 +0100

    updated Hero block patterns

---
+++ patterns/hero-gallery.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Title: Hero (Gallery)
+ * Slug: clarke/hero-gallery
+ * Categories: clarke_hero
+*/
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"},"margin":{"top":"0"}}},"layout":{"contentSize":"1440px","wideSize":"1440px","type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
+
+	<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|80","left":"var:preset|spacing|80"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-center">
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+
+			<!-- wp:heading {"level":1} -->
+			<h1>Let us tell your <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">story</mark>.<br><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">Attract</mark> your audience.<br>Elevate your <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">brand</mark>.</h1>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons">
+
+				<!-- wp:button -->
+				<div class="wp-block-button">
+					<a class="wp-block-button__link wp-element-button">CopyWriting Services</a>
+				</div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"backgroundColor":"medium-gray","textColor":"white","className":"is-style-fill"} -->
+				<div class="wp-block-button is-style-fill">
+					<a class="wp-block-button__link has-white-color has-medium-gray-background-color has-text-color has-background wp-element-button">Contact us</a>
+				</div>
+				<!-- /wp:button -->
+
+			</div>
+			<!-- /wp:buttons -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+
+			<!-- wp:gallery {"linkTo":"none"} -->
+			<figure class="wp-block-gallery has-nested-images columns-default is-cropped">
+
+				<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="">
+				</figure>
+				<!-- /wp:image -->
+
+				<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="">
+				</figure>
+				<!-- /wp:image -->
+
+				<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="">
+				</figure>
+				<!-- /wp:image -->
+
+				<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="">
+				</figure>
+				<!-- /wp:image -->
+
+				<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+				<figure class="wp-block-image size-full">
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="">
+				</figure>
+				<!-- /wp:image -->
+
+			</figure>
+			<!-- /wp:gallery -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
---
+++ patterns/hero-services.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Title: Hero (Services)
+ * Slug: clarke/hero-services
+ * Categories: clarke_hero
+*/
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"},"margin":{"top":"0"}}},"layout":{"contentSize":"1440px","wideSize":"1440px","type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
+
+	<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|80","left":"var:preset|spacing|80"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-center">
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+
+			<!-- wp:heading {"level":1} -->
+			<h1>Let us tell your <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">story</mark>.<br><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">Attract</mark> your audience.<br>Elevate your <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">brand</mark>.</h1>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons">
+
+				<!-- wp:button -->
+				<div class="wp-block-button">
+					<a class="wp-block-button__link wp-element-button">CopyWriting Services</a>
+				</div>
+				<!-- /wp:button -->
+
+				<!-- wp:button {"backgroundColor":"medium-gray","textColor":"white","className":"is-style-fill"} -->
+				<div class="wp-block-button is-style-fill">
+					<a class="wp-block-button__link has-white-color has-medium-gray-background-color has-text-color has-background wp-element-button">Contact us</a>
+				</div>
+				<!-- /wp:button -->
+
+			</div>
+			<!-- /wp:buttons -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":3210,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="http://localhost/wp-content/uploads/2022/03/StockSnap_RV2H4VBE3L-scaled-1-1024x683.jpg" alt="" class="wp-image-3210" style="border-radius:12px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"border":{"radius":"12px"}},"backgroundColor":"gray","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-gray-background-color has-background" style="border-radius:12px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">For individuals</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"id":3213,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="http://localhost/wp-content/uploads/2022/03/StockSnap_YLZ9D6JH3M-scaled-1-1024x680.jpg" alt="" class="wp-image-3213" style="border-radius:12px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"border":{"radius":"12px"}},"backgroundColor":"gray","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-gray-background-color has-background" style="border-radius:12px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="has-large-font-size">For companies</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
