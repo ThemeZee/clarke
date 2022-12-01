4bea06dc68e68954a20ce9e2c7a9e486a1013e73
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2022-12-01 15:40:20 +0100

    updated media text patterns

--- patterns/media-text-left.php
+++ patterns/media-text-left.php
@@ -9,27 +9,41 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"},"margin":{"top":"0"}}},"layout":{"contentSize":"1440px","wideSize":"1440px","type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
 
-	<!-- wp:media-text {"align":"","mediaType":"image"} -->
-	<div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media">
-		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/hero-image.png" alt=""/></figure>
-		<div class="wp-block-media-text__content">
+	<!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns are-vertically-aligned-center">
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"12px"}},"className":"is-style-default"} -->
+			<figure class="wp-block-image size-full has-custom-border is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/portfolio-image.png" alt="" style="border-radius:12px"/>
+			</figure>
+			<!-- /wp:image -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
 
 			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"primary"} -->
-			<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:500">For Companies</p>
+			<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:500">For Freelancers</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-underlined-heading"} -->
-			<h2 class="is-style-underlined-heading" style="margin-top:var(--wp--preset--spacing--20)">Accounting Services<br>and Tax Optimization</h2>
+			<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}}} -->
+			<h2 style="margin-top:var(--wp--preset--spacing--20)">Let us rewrite your copy <br>and get <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">more customers</mark>.</h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis diss parturient.</p>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
 			<!-- /wp:paragraph -->
 
 		</div>
+		<!-- /wp:column -->
 
 	</div>
-	<!-- /wp:media-text -->
+	<!-- /wp:columns -->
 
 </div>
 <!-- /wp:group -->
