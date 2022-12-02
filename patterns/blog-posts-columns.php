d7dd44aec54af370e95384478ae957001a68e6d7
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2022-12-02 15:47:17 +0100

    updated blog and team block patterns

--- patterns/blog-posts-columns.php
+++ patterns/blog-posts-columns.php
@@ -9,8 +9,12 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"},"margin":{"top":"0"},"blockGap":"var:preset|spacing|70"}},"layout":{"contentSize":"1440px","wideSize":"1440px","type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
 
-	<!-- wp:heading {"className":"is-style-underlined-heading"} -->
-	<h2 class="is-style-underlined-heading">Latest News</h2>
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"primary"} -->
+	<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:500">Latest News</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}}} -->
+	<h2 style="margin-top:var(--wp--preset--spacing--20)">Stay in touch with us.</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:query {"queryId":1,"query":{"perPage":"3","pages":"3","offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":3}} -->
@@ -21,13 +25,13 @@
 			<!-- wp:group -->
 			<div class="wp-block-group">
 
-			<!-- wp:post-featured-image {"isLink":true,"height":"","style":{"spacing":{"margin":{"top":"0px"}}}} /-->
+				<!-- wp:post-featured-image {"isLink":true,"style":{"border":{"radius":"12px"}}} /-->
 
-			<!-- wp:pattern {"slug":"clarke/postmeta-date"} /-->
+				<!-- wp:post-terms {"term":"category","style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"500"}},"fontSize":"small"} /-->
 
-			<!-- wp:post-title {"isLink":true,"className":"is-style-underlined-heading","fontSize":"x-large"} /-->
+				<!-- wp:post-title {"isLink":true,"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontSize":"x-large"} /-->
 
-			<!-- wp:post-excerpt {"moreText":"Continue reading"} /-->
+				<!-- wp:post-excerpt {"moreText":"Continue reading"} /-->
 
 			</div>
 			<!-- /wp:group -->
--- patterns/team-columns.php
+++ patterns/team-columns.php
@@ -9,8 +9,12 @@
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","right":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|80"},"margin":{"top":"0"}}},"layout":{"contentSize":"1440px","wideSize":"1440px","type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="margin-top:0;padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--80)">
 
-	<!-- wp:heading {"className":"is-style-underlined-heading"} -->
-	<h2 class="is-style-underlined-heading">Our Team of Experts</h2>
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"primary"} -->
+	<p class="has-primary-color has-text-color" style="font-style:normal;font-weight:500">About us</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}}} -->
+	<h2 style="margin-top:var(--wp--preset--spacing--20)">Meet our dedicated <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-primary-color">team of experts</mark>.</h2>
 	<!-- /wp:heading -->
 
 	<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"},"margin":{"top":"var:preset|spacing|70"}}}} -->
@@ -19,18 +23,82 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full is-style-default">
-				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="">
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+			<figure class="wp-block-image size-full has-custom-border is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="" style="border-radius:12px"/>
 			</figure>
 			<!-- /wp:image -->
 
-			<!-- wp:paragraph -->
-			<p><strong>John Doe</strong><br>Founder &amp; CEO</p>
+			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"white"} -->
+			<p class="has-white-color has-text-color" style="font-style:normal;font-weight:700">John Doe</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:social-links {"size":"has-normal-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-primary-hover"} -->
-			<ul class="wp-block-social-links has-normal-icon-size is-style-primary-hover" style="margin-top:var(--wp--preset--spacing--20)">
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+			<p style="margin-top:0">Founder &amp; CEO</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:social-links {"iconColor":"light-gray","iconColorValue":"#abacad","size":"has-small-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-logos-only"} -->
+			<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--20)">
+			
+				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
+				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
+				<!-- wp:social-link {"url":"https://linkedin.com","service":"linkedin"} /-->
+		
+			</ul>
+			<!-- /wp:social-links -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+			<figure class="wp-block-image size-full has-custom-border is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="" style="border-radius:12px"/>
+			</figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"white"} -->
+			<p class="has-white-color has-text-color" style="font-style:normal;font-weight:700">Jane Smith</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+			<p style="margin-top:0">Online Marketer</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:social-links {"iconColor":"light-gray","iconColorValue":"#abacad","size":"has-small-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-logos-only"} -->
+			<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--20)">
+			
+				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
+				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
+				<!-- wp:social-link {"url":"https://linkedin.com","service":"linkedin"} /-->
+
+			</ul>
+			<!-- /wp:social-links -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+			<figure class="wp-block-image size-full has-custom-border is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="" style="border-radius:12px"/>
+			</figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"white"} -->
+			<p class="has-white-color has-text-color" style="font-style:normal;font-weight:700">Mark Miller</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+			<p style="margin-top:0">Public Relations Specialist</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:social-links {"iconColor":"light-gray","iconColorValue":"#abacad","size":"has-small-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-logos-only"} -->
+			<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--20)">
 
 				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
 				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
@@ -45,70 +113,22 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full is-style-default">
-				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="">
+			<!-- wp:image {"sizeSlug":"full","linkDestination":"none","style":{"border":{"radius":"12px"}}} -->
+			<figure class="wp-block-image size-full has-custom-border is-style-default">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="" style="border-radius:12px"/>
 			</figure>
 			<!-- /wp:image -->
 
-			<!-- wp:paragraph -->
-			<p><strong>Jane Smith</strong><br>Tax Consultant</p>
+			<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"white"} -->
+			<p class="has-white-color has-text-color" style="font-style:normal;font-weight:700">Jill Baker</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:social-links {"size":"has-normal-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-primary-hover"} -->
-			<ul class="wp-block-social-links has-normal-icon-size is-style-primary-hover" style="margin-top:var(--wp--preset--spacing--20)">
-
-				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
-				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
-				<!-- wp:social-link {"url":"https://linkedin.com","service":"linkedin"} /-->
-
-			</ul>
-			<!-- /wp:social-links -->
-
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column">
-
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full is-style-default">
-				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="">
-			</figure>
-			<!-- /wp:image -->
-
-			<!-- wp:paragraph -->
-			<p><strong>Mark Miller</strong><br>Accountant</p>
+			<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+			<p style="margin-top:0">Professional Copywriter</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:social-links {"size":"has-normal-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-primary-hover"} -->
-			<ul class="wp-block-social-links has-normal-icon-size is-style-primary-hover" style="margin-top:var(--wp--preset--spacing--20)">
-
-				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
-				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
-				<!-- wp:social-link {"url":"https://linkedin.com","service":"linkedin"} /-->
-
-			</ul>
-			<!-- /wp:social-links -->
-
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column">
-
-			<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
-			<figure class="wp-block-image size-full is-style-default">
-				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/team-member.png" alt="">
-			</figure>
-			<!-- /wp:image -->
-
-			<!-- wp:paragraph -->
-			<p><strong>Jill Baker</strong><br>Financial Planner</p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:social-links {"size":"has-normal-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-primary-hover"} -->
-			<ul class="wp-block-social-links has-normal-icon-size is-style-primary-hover" style="margin-top:var(--wp--preset--spacing--20)">
+			<!-- wp:social-links {"iconColor":"light-gray","iconColorValue":"#abacad","size":"has-small-icon-size","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"className":"is-style-logos-only"} -->
+			<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only" style="margin-top:var(--wp--preset--spacing--20)">
 
 				<!-- wp:social-link {"url":"mailto://example@email.com","service":"mail"} /-->
 				<!-- wp:social-link {"url":"https://twitter.com","service":"twitter"} /-->
