0fe8e4e808a6a41c358eeb02689c20af3632bc0f
Thomas Weichselbaumer <thomas.weichselbaumer@netzberufler.de>
2022-12-03 13:12:05 +0100

    improved footer block patterns

--- patterns/footer-text.php
+++ patterns/footer-text.php
@@ -6,10 +6,6 @@
 */
 ?>
 
-<!-- wp:paragraph -->
-<p>&copy; <?php echo wp_kses_post( date_i18n( 'Y' ) . ' ' . get_bloginfo( 'name' ) ); ?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|light-gray"}}}},"className":"flip-link-hover"} -->
-<p class="flip-link-hover has-link-color"><a href="#"><?php _e( 'Privacy Policy', 'clarke' ); ?></a> | <a href="#"><?php _e( 'Imprint', 'clarke' ); ?></a></p>
+<!-- wp:paragraph {"className":"flip-link-hover"} -->
+<p class="flip-link-hover">&copy; <?php echo wp_kses_post( date_i18n( 'Y' ) . ' ' . get_bloginfo( 'name' ) ); ?> | <a href="#"><?php _e( 'Privacy Policy', 'clarke' ); ?></a> | <a href="#"><?php _e( 'Imprint', 'clarke' ); ?></a></p>
 <!-- /wp:paragraph -->
